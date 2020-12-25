277f687903afbd3d6d9ab62559fe3f0497d29428
SANJEEV KUMAR <62838848+sanjeev-divine@users.noreply.github.com>
2020-12-25 22:06:05 +0530

    basic commit

---
+++ CRUD.py
@@ -0,0 +1,124 @@
+from time import gmtime,strftime
+from sys import displayhook
+from flask import Flask, json
+from flask import jsonify,request
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from bson.json_util import dumps
+from werkzeug.security import check_password_hash,generate_password_hash
+
+app = Flask(__name__)
+
+app.secret_key = 'secretkey'
+app.config['MONGO_URI'] = "mongodb://localhost:27017/firstdbapi" 
+mongo = PyMongo(app)
+
+
+@app.route('/addnewrecord',methods=['POST'])
+def addrecord():
+
+    json = request.json
+    name = json['name']
+    email = json['email']
+    password = json['password']
+    dateofadding = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+
+    if name and email and password and request.method == 'POST':
+
+        hashpass = generate_password_hash(password)
+        datamap = {'name':name,'email':email,'password':hashpass,'date-of-adding':dateofadding}
+        id = mongo.db.first.insert_one(datamap)
+        respons = jsonify('record added succesfully')
+        respons.status_code = 200
+        return respons
+    
+    else:
+        errorthrow()
+
+
+@app.route('/viewallrecord')
+def viewallrecord():
+    records = mongo.db.first.find()
+    response= dumps(records)
+    return response
+
+
+@app.route('/deleteDocumentbyid=<id>',methods=['DELETE'])
+def deletedocument(id):
+    mongo.db.first.delete_one({'_id':ObjectId(id)})
+    resp = jsonify('Document deleted successfully')
+    resp.status_code = 200
+    return resp
+
+
+@app.route('/view-recordby-id-using-getrequest=<id>')
+def viewrecordbyid(id):
+    data = mongo.db.first.find_one({'_id':ObjectId(id)})
+    data = dumps(data)
+    return data
+
+
+@app.route('/view-recordby-id-using-postrequest',methods=['POST'])
+def recordbyidusingPost():
+    json = request.json
+    id = json['id']
+
+    if id and request.method == 'POST':
+        resp = mongo.db.first.find_one({'_id':ObjectId(id)})
+        resp = dumps(resp)
+
+        if resp == 'null':
+            return notfounderror()
+        else:
+            return resp
+    else:
+        errorthrow()
+
+    
+@app.route('/updateRecord=<id>',methods = ['PUT'])
+def updateRecord(id):
+    mid =id
+    json = request.json
+    name = json['name']
+    email = json['email']
+    password = json['password']
+
+    if name and email and password and request.method == 'PUT':
+        
+        updateddatamap = ({'name':name,'email':email,'password':password})
+        mongo.db.first.update_one({'_id':ObjectId(id['$oid']) if '$oid' in id else ObjectId(id)},{'$set':updateddatamap})
+
+        resp = jsonify('updated succesfully')
+        resp.status_code = 200
+        return resp
+    
+    else:
+        errorthrow()
+
+
+
+
+@app.errorhandler(404)
+def errorthrow(error = None):
+    message = {
+        'status':404,
+        'message': 'not found'+request.url
+            }
+
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
+def notfounderror(error = None):
+    message = {
+        'status':404,
+        'message':'document not found'
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+    return resp
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
